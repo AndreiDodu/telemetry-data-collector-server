aa36dc3463dee5c56f4d3de72e09977e6da1a057
Andrei Dodu <andrei.dodu@hotmail.it>
2021-08-16 19:50:42 +0200

    first version

---
+++ src/main/java/org/isf/telemetry/CollectorController.java
@@ -0,0 +1,29 @@
+package org.isf.telemetry;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CollectorController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Open Hospital data collector server. Make a post request in order to store some data.";
+	}
+
+	@PostMapping(value = "/collect")
+	public Map<String, String> collect(@RequestBody Map<String, Map<String, String>> data) {
+		// TODO store data somewhere
+		System.out.println("Message received!");
+		System.out.println(data.toString());
+		Map<String, String> result = new HashMap<>();
+		result.put("status", "true");
+		return result;
+	}
+
+}
